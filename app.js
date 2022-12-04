220d36a7d1ac5df9241b137f53cab0fcb00666fc
helloasir <helloasir@gmail.com>
2022-12-04 19:12:55 +0400

    bootstrap added

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const path = require('path')
 const exphbs = require('express-handlebars')
 
 
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/styles", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
 
 
 // Initialize built-in middleware for urlencoding and json
