d80a42cb971940ab2bb52a36fd2e118955c0ae15
helloasir <helloasir@gmail.com>
2022-12-04 16:02:00 +0400

    app js updated

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const path = require('path');
-const exphbs = require('express-handlebars');
+const path = require('path')
+const exphbs = require('express-handlebars')
 
 
 app.use(express.static(path.join(__dirname, 'public')));
